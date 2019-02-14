9dff3449d1529ca5d27c32d2d5f790903870dc19
Hanchenxi <306584227@qq.com>
2019-02-14 20:40:45 +0800

    修改测试

--- src/com/vince/ui/LoginClass.java
+++ src/com/vince/ui/LoginClass.java
@@ -10,7 +10,7 @@ public class LoginClass extends BaseClass {
     public void login() throws BusinessException{
         println(getString("input.username"));
         String username = input.nextLine();
-        println(getString("input.password"));
+        println(getString("input.passwords"));
         String password = input.nextLine();
 
         UserService userService = new UserServiceImpl();
