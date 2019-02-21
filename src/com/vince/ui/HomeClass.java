49b0f5b77213742c732def91071aeb0c2427b24c
Hanchenxi <306584227@qq.com>
2019-02-21 16:02:15 +0800

    update product buy and showOrder

--- src/com/vince/ui/HomeClass.java
+++ src/com/vince/ui/HomeClass.java
@@ -10,6 +10,7 @@ import com.vince.service.OrderService;
 import com.vince.utils.BusinessException;
 import com.vince.utils.ConsoleTable;
 import com.vince.utils.DateUtils;
+import com.vince.utils.ProductsXmlUtils;
 
 import java.awt.*;
 import java.util.Date;
@@ -24,6 +25,9 @@ public class HomeClass extends BaseClass{
     public void show(){
         showProducts();
         println("welcome:"+currUser.getUsername());
+        menu();
+    }
+    public void menu(){
         boolean flag = true;
         while(flag){
             println(getString("home.function"));
@@ -31,7 +35,7 @@ public class HomeClass extends BaseClass{
             String select = input.nextLine();
             switch (select){
                 case"1"://1、查询全部订单
-                    findList();
+                    findOrderList();
                     flag = false;
                     break;
                 case "2"://2、查询订单
@@ -46,18 +50,21 @@ public class HomeClass extends BaseClass{
                         println(e.getMessage());
                     }
                     break;
+                case"4"://显示商品
+                    show();
+                    break;
                 case "0"://0、退出
                     flag = false;
+                    println(getString("info.exit"));
                     System.exit(0);
                     break;
 
                     default:
                         println(getString("input.error"));
+                        break;
             }
         }
     }
-    private void findList(){}
-    private void findOrderById(){}
 
     /**
      * 购买商品
@@ -70,13 +77,22 @@ public class HomeClass extends BaseClass{
         int count = 1;
         float sum = 0.0f;//订单总金额
         Order order = new Order();//生成的订单
+        List<Clothes> list = ProductsXmlUtils.perserProductFormXml();//商品列表
         while(flag){
             println(getString("product.input.id"));
             String id = input.nextLine();
-            println(getString("product.input.shoppingNum"));
+            for(Clothes c :list) {
+                if (!id.equals(c.getId())) {
+                    println(getString("input.error"));
+
+                } else {
+                    println(getString("product.input.shoppingNum"));
+                }break;
+            }
             String shoppingNum = input.nextLine();
             OrderItem orderItem = new OrderItem();
             Clothes clothes =clothesService.findById(id);
+
             int num = Integer.parseInt(shoppingNum);
             if(num>clothes.getNum()){
                 throw new BusinessException("product.num.error");
@@ -95,12 +111,15 @@ public class HomeClass extends BaseClass{
             switch (isBuy){
                 case"1":
                     flag = true;
+                    buyProducts();
                     break;
                 case"2":
                     flag= false;
+                    show();
                     break;
                     default:
                         flag= false;
+                        println(getString("input.error"));
                         break;
             }
         }
@@ -110,10 +129,58 @@ public class HomeClass extends BaseClass{
         order.setOrderId(orderService.list().size()+1);
         orderService.buyProduct(order);
         clothesService.update();//更新一下
-        showProducts();//再显示一下
+        show();//再显示一下
 
     }
+    private void findOrderById(){//2、查询订单
+        println(getString("product.order.input.oid"));
+        String oid = input.nextLine();
+        Order order = orderService.findById(Integer.parseInt(oid));
+        if(oid.equals(order.getOrderId())){
+            showOrder(order);//显示订单
+        }else{
+            println(getString("product.order.error"));
+        }
+        menu();//显示菜单
+    }
+    //查询订单列表
+    private void findOrderList(){
+        List<Order> list = orderService.list();
+        for(Order o: list){
+            showOrder(o);
+        }
+        menu();
+    }
 
+
+    private void showOrder(Order o){
+        print(getString("product.order.oid")+o.getOrderId());
+        print("\t"+getString("product.order.createDate")+o.getCreatDate());
+        println("\t"+getString("product.order.sum")+o.getSum());
+        ConsoleTable t= new ConsoleTable(9,true);
+        t.appendRow();
+        t.appendColum("itemid")
+                .appendColum("brand")
+                .appendColum("style")
+                .appendColum("color")
+                .appendColum("size")
+                .appendColum("price")
+                .appendColum("description")
+                .appendColum("shoppingNum")
+                .appendColum("sum");
+        for(OrderItem item : o.getOrderItemList()){
+            t.appendRow();
+            t.appendColum(item.getItemId())
+                    .appendColum(item.getClothes().getBrand())
+                    .appendColum(item.getClothes().getStyle())
+                    .appendColum(item.getClothes().getColor())
+                    .appendColum(item.getClothes().getSize())
+                    .appendColum(item.getClothes().getPrice())
+                    .appendColum(item.getClothes().getDescription());
+        }
+        println(t.toString());
+
+    }
     public void showProducts(){
         List<Clothes> list = clothesService.list();
 
--- src/com/vince/utils/BusinessException.java
+++ src/com/vince/utils/BusinessException.java
@@ -1,6 +1,7 @@
 package com.vince.utils;
 /*
 异常类
+自定义异常
 注册失败 运行期异常
  */
 public class BusinessException extends RuntimeException {
